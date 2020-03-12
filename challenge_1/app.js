7c607d488ca17b2bbac233f29d53ac68ddc86297
Minh <minhvn29@gmail.com>
2020-03-11 19:25:44 -0700

    Create flexbox for gameboard

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,3 +1,44 @@
 
 
 console.log('connected!');
+
+/* TO DOs
+- build board
+- function to detect a tie
+- reset button (handle onClick event)
+
+- rules of the game
+- alternate between x and o player, keep track of how many placed onto board and swap per play
+- after each play, check if diagonal or row or column filled up
+- if yes, display winner 
+- if no and board is filled up, there is a tie, display tie
+
+*/
+// set up an object to hold states
+
+// set up new board
+initializeGame = function() {
+
+}
+
+// 
+
+// helper functions to check for a win
+checkRowFilled = function() {
+
+}
+
+checkColumnFilled = function() {
+
+}
+
+checkMajorDiagonalFilled = function() {
+
+}
+
+checkMinorDiagonalFilled = function() {
+
+}
+
+
+
