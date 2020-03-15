c8ddd14010856e685a73175893bfd76d243dd912
Minh <minhvn29@gmail.com>
2020-03-15 15:46:18 -0700

    Complete html formatting

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
-const app = express()
+const app = express();
 const port = 3000;
+const bodyParser = require('body-parser');
 
+app.use(bodyParser);
 app.use(express.static('client'));
+
 
 app.listen(port, () => console.log('Listening on port 3000...'));
 
