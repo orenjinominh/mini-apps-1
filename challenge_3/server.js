d05fbab27c909876ccd9a6efdcec91933aaf965d
Minh <minhvn29@gmail.com>
2020-03-18 20:30:17 -0700

    Complete Express and React hookups and base templating

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,14 @@
+/*-----------constants------------*/
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
 
+/*-----------middleware------------*/
 
+app.use(express.static('public'));
+
+/*-----------routing--------------*/
+
+/*-----------initializing------------*/
+
+app.listen(port, ()=> console.log('Listening on port 3000'));
