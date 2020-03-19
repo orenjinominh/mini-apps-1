7874f75a728ccecde61a9ccf820105772b8eb384
Minh <minhvn29@gmail.com>
2020-03-19 10:06:25 -0700

    Create Checkout component with form and next handler functions

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,1 +1,88 @@
-// build components go here 
+class Checkout extends React.Component {
+
+  // set up states that will change when forms are input and submitted
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      form: 'checkout',
+      userData: {
+        key: '',
+        value: ''
+      }
+    };
+
+    // state changes when form value changes and 'next' button clicked 
+    this.handleFormChange = this.handleFormChange.bind(this);
+    this.handleNextClick = this.handleNextClick.bind(this);
+  }
+
+  // this should change userData state, bound to form change event 
+  handleFormChange(event) {
+    let key = event.target.id;
+    let value = event.target.value; 
+
+    let userDataCopy = {...this.state.userData};
+    userDataCopy[key] = value; 
+    this.setState({userDataCopy});
+
+    // this.setState({
+    //   userData: {...this.state.userData,key,value}
+    // })
+  }
+
+  // this should change the form state, bound to next button click 
+  handleNextClick() {
+    if (this.state.form === 'checkout') {
+      this.enterData();
+      this.setState({
+        form: 'userInfo'
+      });
+    } else if (this.state.form === 'userInfo') {
+      this.enterData();
+      this.setState({
+        form: 'address'
+      });
+
+    } else if (this.state.form === 'address') {
+      this.enterData();
+      this.setState({
+        form: 'creditCard'
+      });
+    } else if (this.state.form === 'creditCard') {
+      this.enterData();
+      this.setState({
+        form: 'reviewPurchase'
+      });
+    }
+  }
+
+  // helper ajax function to post to database with click on "next"
+  enterData() {
+
+  }
+
+  // function to take user to homepage (and save data to db?) with ajax call
+  submitPurchase() {
+    // remove purchase button 
+    // reset state to normal 
+  }
+
+  render() {
+
+
+
+
+
+
+  }
+
+
+
+
+
+}
+
+
+
+ReactDOM.render(<Checkout />, document.getElementById('app'));
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -2,13 +2,16 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
 
 /*-----------middleware------------*/
 
 app.use(express.static('public'));
+app.use(bodyParser());
 
 /*-----------routing--------------*/
 
+
 /*-----------initializing------------*/
 
-app.listen(port, ()=> console.log('Listening on port 3000'));
+app.listen(port, () => console.log('Listening on port 3000'));
